3f3075de9ecd61aa23198ee5bfbb72d616849403
Thayná <thaynavazzoler.564@gmail.com>
2023-08-18 10:18:13 -0300

    feat: create classes

---
+++ assets/js/script.category-product.js
@@ -0,0 +1,38 @@
+//Cada categorias possuei vários produtos
+//Cada produto é pertencente a uma Categoria
+
+class Category{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.products = [];
+    }
+}
+
+class products{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 1;
+    }
+    addCategory(name){
+        const id = this.nextCategoryId;
+        this.nextCategoryId++;
+
+        const category = new Category(id,name);
+        this.categories.push(category);
+    }
+}
+
+const categoriesList = new CategoryService();
+function createCategory(){
+
+}
