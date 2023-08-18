3c9c1ff5fdce27f63c3f3cbc9517fffe3cf22ef8
Thayná <thaynavazzoler.564@gmail.com>
2023-08-18 15:13:44 -0300

    feat: create deleteCategory function

--- assets/js/script.category-product.js
+++ assets/js/script.category-product.js
@@ -114,3 +114,9 @@ function editCategory(id, name){
 
     console.log(categoriesList.categories);
 }
+
+function deleteCategory(id){
+    categoriesList.deleteCategory(id);
+
+    console.log(categoriesList.categories);
+}
